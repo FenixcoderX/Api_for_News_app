a27e5c1140254f9abb8734ba29d864e636475bc5
FenixcoderX <fenixcoder2024@gmail.com>
2024-06-24 19:33:20 +0700

    Add connection to database

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,35 @@
-
 import express, { NextFunction, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 // Create a new express server
 const app: express.Application = express();
 const address: string = '0.0.0.0:3001';
 
-// Define a route handler for the default home page
-app.get('/', (req: Request, res: Response) => {
-    res.send('This is API');
-  });
-  
-// Start the Express server
-app.listen(3001, () => {
-    console.log(`starting app on: ${address}`);
+// Load the environment variables from the .env file into process.env
+dotenv.config();
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO as string)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
   });
 
-  export default app;
+// Extracts the entire body portion of an incoming request stream and exposes it on req.body
+app.use(express.json());
+
+// Define a route handler for the default home page
+app.get('/', (req: Request, res: Response) => {
+  res.send('This is API');
+});
+
+// Start the Express server
+app.listen(3001, () => {
+  console.log(`starting app on: ${address}`);
+});
+
+export default app;
